1642caae4d7fb5284f8715f6ee9b9a2c3a4fe464
jungRoit <roit.github@gmail.com>
2020-01-02 12:50:40 +0545

    token verification added for protected routes

--- src/middlewares/checkAuthorization.js
+++ src/middlewares/checkAuthorization.js
@@ -1,12 +1,24 @@
 import getTokenFromHeader from '../utils/getTokenFromHeader';
 
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default (req, res, next) => {
   const token = getTokenFromHeader(req);
   if (!token) {
     res.status(401).send({
-      error: 'Invalid Token'
+      error: 'Empty Token'
     });
   } else {
-    next();
+    try {
+      jwt.verify(token, process.env.SECRET_KEY);
+      next();
+    } catch (error) {
+      res.status(401).send({
+        message: 'Invalid Token',
+        error: error
+      });
+    }
   }
 };
