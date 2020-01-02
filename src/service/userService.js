d5ef4a6b8213acaf97682702dbadc9ee59abd420
jungRoit <roit.github@gmail.com>
2020-01-02 14:39:58 +0545

    generic dao created

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,21 +1,25 @@
-import * as UsersDao from '../dao/usersDao';
+import * as genericDao from '../dao/genericDao';
 
 export const getAllUsers = async () => {
-  return await UsersDao.getAllUsers();
+  return await genericDao.getAll('users');
 };
 
 export const getUserById = async id => {
-  return await UsersDao.getUserById(id);
+  return await genericDao.getBy('users', { id: id });
 };
 
 export const getUserByUsername = async username => {
-  return await UsersDao.getUserByUsername(username);
+  return await genericDao.getBy('users', { username: username });
 };
 
 export const insertUser = async user => {
-  return await UsersDao.insertUser(user);
+  return await genericDao.insert('users', user);
 };
 
 export const updateLastLogin = async user => {
-  return await UsersDao.updateLastLogin(user);
+  return await genericDao.update(
+    'users',
+    { lastLogin: user.lastLogin },
+    { id: user.id }
+  );
 };
