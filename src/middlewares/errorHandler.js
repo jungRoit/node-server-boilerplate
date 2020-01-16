beee8c8a0893be766a4ac7e79fd4b85a545bb3db
jungRoit <roit.github@gmail.com>
2020-01-16 11:53:21 +0545

    error stack trace added to error response

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,18 @@
 export const genericErrorHandler = (Error, req, res, next) => {
+  const stack = Error.stack.split('\n');
+  stack.splice(0, 1);
   if (Error.message && Error.code) {
     const error = {
       message: Error.message,
-      details: Error.details
+      details: Error.details,
+      trace: stack
     };
     res.status(Error.code).json(error);
   } else {
     const error = {
       message: 'Internal Server Error',
-      details: Error.toString()
+      details: Error.toString(),
+      trace: stack
     };
     res.status(500).json(error);
   }
