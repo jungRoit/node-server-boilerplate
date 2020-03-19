dd72f9801222aa2bebc9aa7f30034e5f0358196d
jungRoit <roit.github@gmail.com>
2020-03-19 15:28:00 +0545

    mocha get all users test added

--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,14 @@
 import { sum } from './sum';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
 
+import app from '../index';
+
+chai.use(chaiHttp);
+
+/**
+ * Test to Test sum function
+ */
 describe('sum function', () => {
   it('sums up two number', () => {
     expect(sum(1, 2)).to.eql(3);
